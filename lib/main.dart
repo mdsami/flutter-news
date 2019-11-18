7068f718e4d5cf1d6a7d0507ae8d2749cb54274d
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-11-19 02:19:01 +0800

    Add license

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_news/home_screen.dart';
 
