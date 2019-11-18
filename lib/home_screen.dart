8f49cb31ce66636e2aa6516965044ad3398d781f
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-11-19 01:46:40 +0800

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
